1ed98a1448976730ffefb7dd02c89cb8c495fcd3
kartikay1506 <kartikaysawhney150699@gmail.com>
2021-09-11 00:36:31 +0530

    added contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to the Go Server.</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to the Go Server.</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:cooperalex512@gmail.com\">cooperalex512@gmail.com</a>")
+	}
 }
 
 func main() {
